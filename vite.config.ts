287b2bf1b6c29fcff76563067e9d08e9fe2a8227
Tatyana Melnikova <neonatura01@yandex.ru>
2025-08-28 17:05:54 +0800

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/4.2.4_VacancysList_MelnikovaTV/",
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/setupTests.ts",
-    css: true,
   },
-  base: "/4.2.4_VacancysList_MelnikovaTV/",
 });
