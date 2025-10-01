00b7144786bdb09a9b301d590217f7833e12959b
Tatyana Melnikova <neonatura01@yandex.ru>
2025-10-01 20:55:33 +0800

    исправлены ошибки фетча

--- src/api/fetchVacancies.ts
+++ src/api/fetchVacancies.ts
@@ -14,8 +14,11 @@ export const fetchVacancies = async (
   url.searchParams.append("page", String(page - 1));
   url.searchParams.append("per_page", "10");
 
-  if (searchValue.trim().length > 0 || skills.length > 0) {
-    const query = [searchValue.trim(), ...(skills || [])].filter(Boolean);
+  const safeSearch = (searchValue || "").trim();
+  const hasSearch = safeSearch.length > 0 || skills.length > 0;
+
+  if (hasSearch) {
+    const query = [searchValue.trim() || [], ...(skills || [])].filter(Boolean);
     url.searchParams.append("search_field", "name");
     url.searchParams.append("search_field", "company_name");
     url.searchParams.append("text", `${query.join(" OR ")}`);
